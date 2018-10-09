cdaff8d71536ded889d0384cefd606ac76428d6f
Rexman17 <a.stone1794@gmail.com>
2018-10-09 16:35:16 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title # getter
+    @title
+  end
 
+  def author=(author) # setter
+    @author = author
+  end
+
+  def author # getter
+    @author
+  end
+
+  def page_count=(page_count) # setter
+    @page_count = page_count
+  end
+
+  def page_count # getter
+    @page_count
+  end
+
+  def genre=(genre) # setter
+    @genre = genre
+  end
+
+  def genre # getter
+    @genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,52 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand) # initialize shoe w a brand
+    @brand = brand
+  end
+
+  def brand=(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material # equals arg passed in
+  end
+
+  def material
+    @material # just GET/READ the material
+  end
+
+  def condition=(state_of_shoe)
+    @condition = state_of_shoe
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
+  end
+
+end
