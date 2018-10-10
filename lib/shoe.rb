a0fb1ea2ea7e58bc412aeef978032759d7d99c84
github username <github email address>
2018-10-09 20:58:13 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
 # Make your shoe class here!
+# 7) Shoe ::new gets initialized with a brand
+# 8) Shoe properties has a brand
+# 9) Shoe properties has a color
+# 10) Shoe properties has a size
+# 11) Shoe properties has a material
+# 12) Shoe properties has a condition
+
+class Shoe
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+# 13) Shoe #cobble says that the shoe has been repaired
+# 14) Shoe #cobble makes the shoe's condition new
